90913965ec59f1381ada7d2afee0f8c755c57d7f
José Mendes <ze7able@gmail.com>
2024-03-05 14:01:42 +0100

    Example Circle

---
+++ src/main/scala/com/Draw.scala
@@ -0,0 +1,21 @@
+package com
+
+import scala.math.{cos, sin, Pi}
+
+class Draw {
+  val numPoints: Int = 1000
+
+  def makeCircle(radius: Double, center: Array[Double]): Array[(Double, Double)] = {
+    val points = new Array[(Double, Double)](numPoints)
+    val thetaIncrement = 2 * Pi / numPoints
+
+    for (i <- 0 until numPoints) {
+      val theta = i * thetaIncrement
+      val x = center(0) + radius * cos(theta)
+      val y = center(1) + radius * sin(theta)
+      points(i) = (x, y)
+    }
+
+    points
+  }
+}
--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -1,33 +1,25 @@
-package com
-
 import scala.swing._
-import scala.swing.event.ButtonClicked
+import java.awt.{ Color}
+import com.PixelGrid
+import com.Draw
 
 object Main extends SimpleSwingApplication {
   def top: Frame = new MainFrame {
-    title = "Simple Swing Demo"
-    preferredSize = new Dimension(300, 200)
+    title = "Pixel Grid Demo"
+    preferredSize = new Dimension(800, 800)
 
-    val button: Button = new Button {
-      text = "Click me!"
-    }
-
-    val label: Label = new Label {
-      text = "Button not clicked yet."
-    }
+    val pixelGrid: PixelGrid = new PixelGrid
+    val draw = new Draw
+    val circle = draw.makeCircle(50, Array(50, 50))
 
     contents = new BoxPanel(Orientation.Vertical) {
-      contents += button
       contents += Swing.VStrut(10)
-      contents += label
+      contents += pixelGrid
       border = Swing.EmptyBorder(10, 10, 10, 10)
     }
 
-    listenTo(button)
-
-    reactions += {
-      case ButtonClicked(`button`) =>
-        label.text = "Button clicked!"
+    circle.foreach { case (x, y) =>
+      pixelGrid.updatePixel(x, y, Color.BLACK)
     }
   }
 }
